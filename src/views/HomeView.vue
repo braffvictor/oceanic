7fb2e5b189338e201e422737e5a2a64affdf39a6
swaedavid <swaedavid@gmail.com>
2024-05-09 05:08:01 +0100

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div
-    class="home bg-slate-100 h-screen dark:bg-slate-900 text-center mx-auto transition-all ease-linear duration-500 peer/red"
-  >
-    <!-- <button
-    class="p-4 transition-all ease-linear duration-500 border-blue-300 bg-blue-100 box-content outline-none peer-hover/red:!text-purple-900 dark:bg-slate-600 dark:text-white"
-    >
-    red
-  </button> -->
-    <DButton class="mt-12" type="filled" to="/about"> Register With Us</DButton>
-    <div
-      class="mt-12 bg-[url('https://solanart.io/_next/image?url=https%3A%2F%2Fapi-v2.solanart.io%2Fcdn%2F500%2Fhttps%3A%2F%2Farweave.net%2Fjm6wD4dL6h-ngCdATOmufSbaYLXsAzCseyOveXnkYk8&w=1920&q=75')] w-full h-full bg-no-repeat"
-    >
-      <svg-comp />
-    </div>
+  <div class="bg-slate-100 h-[150vw] dark:bg-slate-900 transit">
+    <section>
+      <main class="mt-5 md:px-8 relative">
+        <div class="h-[500px] absolute md:rounded-xl blurBg w-[95%]"></div>
+        <div
+          class="h-[500px] md:border md:rounded-xl absolute text-slate-100 w-[95%] backdrop-blur-lg"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          corrupti optio ut et culpa consectetur fugit numquam quaerat
+          excepturi. Quod obcaecati pariatur nobis?
+        </div>
+      </main>
+    </section>
   </div>
 </template>
 
@@ -21,3 +20,17 @@ import DButton from "@/components/utils/DButton.vue";
 import svgComp from "@/components/svgComp.vue";
 // @ is an alias to /src
 </script>
+
+<style>
+.blurBg {
+  background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.247),
+      rgba(0, 0, 0, 0.416)
+    ),
+    url(https://solanart.io/_next/image?url=https%3A%2F%2Fapi-v2.solanart.io%2Fcdn%2F500%2Fhttps%3A%2F%2Farweave.net%2Fjm6wD4dL6h-ngCdATOmufSbaYLXsAzCseyOveXnkYk8&w=1920&q=75);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pwa: {
+    manifestOptions: {
+      name: "Oceanic",
+      short_name: "Oceans",
+      display: "standalone",
+      theme_color: "#4ade80",
+    },
+  },
+});
