e9b36ea6faed9229db9ab6bbdf98abaccbe83228
swaedavid <swaedavid@gmail.com>
2024-05-09 05:12:47 +0100

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <div class="bg-slate-100 h-[150vw] dark:bg-slate-900 transit">
     <section>
       <main class="mt-5 md:px-8 relative">
-        <div class="h-[500px] absolute md:rounded-xl blurBg w-[95%]"></div>
+        <div
+          class="h-[500px] absolute md:rounded-xl blurB dark:blurBg w-[95%]"
+        ></div>
         <div
           class="h-[500px] md:border md:rounded-xl absolute text-slate-100 w-[95%] backdrop-blur-lg"
         >
@@ -27,6 +29,17 @@ import svgComp from "@/components/svgComp.vue";
       to bottom,
       rgba(0, 0, 0, 0.247),
       rgba(0, 0, 0, 0.416)
+    ),
+    url(https://solanart.io/_next/image?url=https%3A%2F%2Fapi-v2.solanart.io%2Fcdn%2F500%2Fhttps%3A%2F%2Farweave.net%2Fjm6wD4dL6h-ngCdATOmufSbaYLXsAzCseyOveXnkYk8&w=1920&q=75);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.blurB {
+  background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.216),
+      rgba(255, 255, 255, 0.253)
     ),
     url(https://solanart.io/_next/image?url=https%3A%2F%2Fapi-v2.solanart.io%2Fcdn%2F500%2Fhttps%3A%2F%2Farweave.net%2Fjm6wD4dL6h-ngCdATOmufSbaYLXsAzCseyOveXnkYk8&w=1920&q=75);
   background-position: center;
