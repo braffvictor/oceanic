a6ecc7f0997892545c94d90012a9252b58ce8fd6
swaedavid <swaedavid@gmail.com>
2024-06-14 17:32:10 +0100

    updated

--- src/components/cards/homeCard.vue
+++ src/components/cards/homeCard.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="">
     <div
-      class="rounded-2xl hover:-translate-y-2 group shadow-lg min-w-56 min-h-72 max-w-56 max-h-72 bg-slate-50 dark:bg-slate-800 overflow-hidden transit cursor-pointer"
+      :class="cardSize"
+      class="rounded-2xl hover:-translate-y-2 group shadow-lg bg-slate-50 dark:bg-slate-800 overflow-hidden transit cursor-pointer"
     >
       <div class="min-h-36 max-h-36 w-full overflow-hidden">
         <img
@@ -24,17 +25,19 @@
             class="max-w-5 inline pl-1"
           />
         </p>
+
         <div
           class="flex justify-between text-slate-900 font-bold dark:text-slate-100 mt-5"
+          v-if="!learn && !nft.action && nft.stats"
         >
-          <div v-if="!learn">
+          <div v-if="!learn && nft && !nft.action">
             <p class="text-gray-400 font-light">Floor</p>
             <p>
               {{ nft && nft.stats.floor_price
               }}{{ nft && nft.stats.floor_price_symbol }}
             </p>
           </div>
-          <div v-if="!learn">
+          <div v-if="!learn && nft && !nft.action">
             <p class="text-gray-400 font-light">Volume</p>
             <p>
               {{
@@ -46,11 +49,33 @@
           </div>
         </div>
       </div>
+      <div
+        v-if="nft && nft.action"
+        class="bg-green-400 p-1 dark:bg-green-500 items-center w-full flex divide-x h-10 justify-around translate-y-24 md:translate-y-32 group-hover:md:translate-y-12 group-hover:translate-y-4 group-hover:h-10 overflow-hidden transit"
+      >
+        <p
+          class="text-slate-900 dark:text-slate-100 transit active:scale-90 text-xs md:text-md"
+        >
+          <!-- use for price data for deduct function -->
+          <!-- {{ nft && nft.stats.floor_price
+          }}{{ nft && nft.stats.floor_price_symbol }} -->
+          But At {{ nft && nft.stats.floor_price.slice(0, 5)
+          }}{{ nft && nft.stats.floor_price_symbol }}
+        </p>
+        <div class="px-2">
+          <svgComp
+            Sclass="active:stroke-green-400 active:scale-90"
+            icon="M8.41799 3.25089C8.69867 2.65917 9.30155 2.25 10 2.25H14C14.6984 2.25 15.3013 2.65917 15.582 3.25089C16.2655 3.25586 16.7983 3.28724 17.2738 3.47309C17.842 3.69516 18.3362 4.07266 18.6999 4.56242C19.0668 5.0565 19.2391 5.68979 19.4762 6.56144L20.2181 9.28272L20.4985 10.124C20.5065 10.1339 20.5144 10.1438 20.5222 10.1539C21.4231 11.3076 20.9941 13.0235 20.1362 16.4553C19.5905 18.638 19.3176 19.7293 18.5039 20.3647C17.6901 21.0001 16.5652 21.0001 14.3153 21.0001H9.68462C7.43476 21.0001 6.30983 21.0001 5.49605 20.3647C4.68227 19.7293 4.40943 18.638 3.86376 16.4553C3.00581 13.0235 2.57684 11.3076 3.47767 10.1539C3.48555 10.1438 3.4935 10.1338 3.50152 10.1239L3.7819 9.28271L4.52384 6.56145C4.76092 5.6898 4.93316 5.0565 5.30009 4.56242C5.66381 4.07266 6.15802 3.69516 6.72621 3.4731C7.20175 3.28724 7.73447 3.25586 8.41799 3.25089ZM8.41951 4.75231C7.75763 4.759 7.49204 4.78427 7.27224 4.87018C6.96629 4.98976 6.70018 5.19303 6.50433 5.45674C6.32822 5.69388 6.22488 6.0252 5.93398 7.09206L5.36442 9.18091C6.38451 9.00012 7.77753 9.00012 9.68462 9.00012H14.3153C16.2224 9.00012 17.6155 9.00012 18.6356 9.18092L18.066 7.09206C17.7751 6.0252 17.6718 5.69388 17.4957 5.45674C17.2998 5.19303 17.0337 4.98976 16.7278 4.87018C16.508 4.78427 16.2424 4.759 15.5805 4.75231C15.2992 5.3423 14.6972 5.75 14 5.75H10C9.30281 5.75 8.70084 5.3423 8.41951 4.75231Z"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import svgComp from "../svgComp.vue";
+
 const props = defineProps({
   nft: {
     type: Object,
@@ -62,9 +87,13 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  cardSize: {
+    type: String,
+    default: "min-w-56 min-h-72 max-w-56 max-h-72",
+  },
 });
 
-console.log(props.nft);
+// console.log(props.nft);
 </script>
 
 <style></style>
--- src/views/collection/collectionView.vue
+++ src/views/collection/collectionView.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
+  <div class="transit">
     <section
-      class="flex items-end p-4 md:p-8 bg-[url('https://i.pinimg.com/736x/fc/98/08/fc9808ed756f6b11956c3983ae5412c6.jpg')] min-h-96 bg-no-repeat bg-center bg-cover -mt-20"
+      class="flex items-end p-4 md:p-8 bg-[url('https://i.seadn.io/gae/WRcl2YH8E3_7884mcJ0DRN7STGqA8xZQKd-0MFmPftlxUR6i1xB9todMXRW2M6SIpXKAZ842UqKDm1UrkKG8nr7l9NjCkIw-GLQSFQ')] min-h-96 bg-no-repeat bg-center bg-cover -mt-20"
     >
-      <main>
-        <div class="flex justify-between md:min-w-[75em]">
+      <main class="w-full">
+        <div class="flex flex-col md:flex-row md:justify-between">
           <div>
             <img
-              src="https://i.seadn.io/s/raw/files/9daac5638c64f69a4e0b0f43aa6f7990.gif?w=500&auto=format"
+              src="https://i.seadn.io/gae/_R4fuC4QGYd14-KwX2bD1wf-AWjDF2VMabfqWFJhIgiN2FnAUpnD5PLdJORrhQ8gly7KcjhQZZpuzYVPF7CDSzsqmDh97z84j2On?w=500&auto=format"
               class="rounded-xl min-w-24 max-w-24"
               alt=""
             />
             <p class="mt-3 md:mt-5 font-bold text-slate-100 text-2xl">
-              Wolf Game (Blast) - Gems<img
+              Mocaverse<img
                 src="@/assets/verified.svg"
                 alt="tick"
                 class="max-w-5 inline pl-1"
               />
             </p>
             <p class="text-gray-300">
-              WolfGameBlastDeployer<img
+              mocaverse<img
                 src="@/assets/verified.svg"
                 alt="tick"
                 class="max-w-5 inline pl-1"
@@ -27,22 +27,157 @@
             </p>
           </div>
 
-          <div class="md:flex gap-x-3 items-end hidden">
+          <div class="flex flex-row gap-x-3 items-end">
             <div
-              v-for="n in 5"
+              v-for="n in 3"
               :key="n"
-              class="text-slate-100 font-bold text-lg"
+              class="text-slate-100 text-sm font-semibold md:font-bold md:text-lg"
             >
               <p>33 ETH</p>
-              <p class="font-light">Total Volume</p>
+              <p class="font-light text-xs">Total Volume</p>
             </div>
           </div>
         </div>
       </main>
     </section>
+
+    <section class="p-4 md:p-8 bg-slate-50 dark:bg-slate-900 transit">
+      <p
+        class="text-slate-900 dark:text-slate-100 text-md md:text-lg transit"
+        :class="detailed ? 'line-clamp-none' : 'line-clamp-2'"
+        @click="detailed = !detailed"
+      >
+        BEANZ are a small species that sprouts from the dirt in the garden. They
+        make for a great sidekick to an Azuki, although some like to kick it
+        alone. They're earnestly driven by the desire to help. However, certain
+        BEANZ feel a calling to pave their own path...
+      </p>
+      <p class="text-slate-900 dark:text-slate-100 text-md md:text-lg mt-3">
+        Items <strong>15.5K</strong> · Created <strong>May 2024</strong> ·
+        Category <strong>Gaming</strong> · Chain <strong>Blast</strong>
+      </p>
+
+      <!-- the button to control component -->
+      <div class="flex mt-3 gap-x-4">
+        <DButton
+          class="!bg-gray-200 dark:!bg-gray-700 !text-slate-900 dark:!text-slate-100 font-semibold"
+          >Overview</DButton
+        >
+        <DButton
+          class="!bg-gray-200 dark:!bg-gray-700 !text-slate-900 dark:!text-slate-100 font-semibold"
+          >Items</DButton
+        >
+        <DButton
+          class="!bg-gray-200 dark:!bg-gray-700 !text-slate-900 dark:!text-slate-100 font-semibold"
+          >Activity</DButton
+        >
+      </div>
+
+      <!-- the items components -->
+      <section>
+        <div
+          class="h-3 w-3 mt-5 animate-pulse rounded-full bg-green-400 dark:bg-green-500 inline-block"
+        ></div>
+        <span class="ml-2 text-slate-900 dark:text-slate-100"
+          >Live 15,508 results</span
+        >
+        <!-- the search item bar -->
+        <div
+          class="mt-2 p-2 flex bg-gray-200 md:w-2/3 dark:text-slate-100 text-slate-900 overflow-hidden dark:bg-slate-700 dark:caret-slate-100 h-11 justify-self-start inline-block align-start rounded-2xl indent-5 hover:ring-green-400 hover:ring-1 outline-none transit"
+        >
+          <SvgComp
+            icon="M10.5,4 C6.91015,4 4,6.91015 4,10.5 C4,14.0899 6.91015,17 10.5,17 C14.0899,17 17,14.0899 17,10.5 C17,6.91015 14.0899,4 10.5,4 Z M2,10.5 C2,5.80558 5.80558,2 10.5,2 C15.1944,2 19,5.80558 19,10.5 C19,12.4869 18.3183,14.3145 17.176,15.7618 L20.8284,19.4142 C21.2189,19.8047 21.2189,20.4379 20.8284,20.8284 C20.4379,21.2189 19.8047,21.2189 19.4142,20.8284 L15.7618,17.176 C14.3145,18.3183 12.4869,19 10.5,19 C5.80558,19 2,15.1944 2,10.5 Z M9.5,7 C9.5,6.44772 9.94772,6 10.5,6 C12.9853,6 15,8.01472 15,10.5 C15,11.0523 14.5523,11.5 14,11.5 C13.4477,11.5 13,11.0523 13,10.5 C13,9.11929 11.8807,8 10.5,8 C9.94772,8 9.5,7.55228 9.5,7 Z"
+          />
+          <input
+            type="text"
+            placeholder="Search Nft..."
+            class="w-full bg-transparent outline-none ml-2"
+          />
+        </div>
+
+        <div>
+          <div
+            class="grid grid-cols-2 md:grid-cols-5 mt-7 gap-3"
+            v-if="nftApiCollection.length > 0"
+          >
+            <HomeCard
+              v-for="nft in collectionNfts.slice(0, 20)"
+              :key="nft.name"
+              :nft="nft"
+              :card-size="'min-w-40 min-h-64 max-w-40 max-h-64 md:min-w-56 md:min-h-72 md:max-w-56 md:max-h-72'"
+            />
+          </div>
+        </div>
+      </section>
+    </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HomeCard from "@/components/cards/homeCard.vue";
+import SvgComp from "@/components/svgComp.vue";
+import DButton from "@/components/utils/DButton.vue";
+import { onMounted, ref } from "vue";
+
+const detailed = ref(false);
+
+const nftApiCollection = ref([]);
+
+const getNftCollection = (chain) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": "u4ryqv9WRFAu5PtwzFHFIHGnyGF8xY26",
+    },
+  };
+
+  fetch(
+    `https://api.blockspan.com/v1/exchanges/collections?chain=${chain}&exchange=opensea&page_size=25`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      // console.log(response)
+      nftApiCollection.value = response.results;
+      nftApiCollection.value.forEach((nft) => {
+        nft.action = "red";
+      });
+
+      // console.log(nftApiCollection.value);
+    })
+    .catch((err) => console.error(err));
+};
+onMounted(() => {
+  getNftCollection("eth-main");
+  specificCollectionNfts();
+});
+const collectionNfts = ref([]);
+const specificCollectionNfts = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "x-api-key": "6db12e6a6438461e9e3755c8b8930c21",
+    },
+  };
+
+  fetch("https://api.opensea.io/api/v2/collection/beanzofficial/nfts", options)
+    .then((response) => response.json())
+    .then((response) => {
+      collectionNfts.value = response.nfts;
+
+      collectionNfts.value.forEach((nft) => {
+        nft.action = "red";
+        nft.stats = {
+          floor_price: "0.0034",
+          floor_price_symbol: "ETH",
+        };
+      });
+      // console.log(collectionNfts.value);
+    })
+    .catch((err) => console.error(err));
+};
+</script>
 
 <style scoped></style>
