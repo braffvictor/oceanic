2c9e57fddbe8636812b2b538c31dfbf44e0056ba
swaedavid <swaedavid@gmail.com>
2024-06-14 04:02:36 +0100

    updated

--- src/views/collection/collectionView.vue
+++ src/views/collection/collectionView.vue
@@ -4,8 +4,8 @@
       class="flex items-end p-4 md:p-8 bg-[url('https://i.pinimg.com/736x/fc/98/08/fc9808ed756f6b11956c3983ae5412c6.jpg')] min-h-96 bg-no-repeat bg-center bg-cover -mt-20"
     >
       <main>
-        <div>
-          <div class="">
+        <div class="flex justify-between md:min-w-[75em]">
+          <div>
             <img
               src="https://i.seadn.io/s/raw/files/9daac5638c64f69a4e0b0f43aa6f7990.gif?w=500&auto=format"
               class="rounded-xl min-w-24 max-w-24"
@@ -25,6 +25,17 @@
                 class="max-w-5 inline pl-1"
               />
             </p>
+          </div>
+
+          <div class="md:flex gap-x-3 items-end hidden">
+            <div
+              v-for="n in 5"
+              :key="n"
+              class="text-slate-100 font-bold text-lg"
+            >
+              <p>33 ETH</p>
+              <p class="font-light">Total Volume</p>
+            </div>
           </div>
         </div>
       </main>
