eccb56c72f3f1380f6bc0728391207d4afa1a30e
swaedavid <swaedavid@gmail.com>
2024-07-02 16:55:35 +0100

    updated

--- src/views/learn/what-are-nfts.vue
+++ src/views/learn/what-are-nfts.vue
@@ -1,9 +1,267 @@
 <template>
-  <div>
-    <section></section>
+  <div
+    class="bg-gradient-to-b from-green-100 transit via-purple-100 to-slate-50 dark:from-green-300 dark:to-slate-900 min-h-screen to-100%%"
+  >
+    <section
+      class="mx-auto transit text-center md:text-left text-slate-900 dark:text-slate-50 p-2 w-11/12 md:w-7/12"
+    >
+      <!-- top text -->
+      <div>
+        <p
+          class="text-slate-900 dark:text-slate-50 font-light md:text-left text-xl"
+        >
+          NFT
+        </p>
+        <p class="text-4xl md:text-6xl font-semibold">What is an NFT?</p>
+        <p class="font-thin text-sm">August 26, 2022</p>
+      </div>
+
+      <!-- image -->
+      <div
+        class="w-full h-full rounded-3xl mt-4 md:mt-7 overflow-hidden shadow-lg shadow-blue-400"
+      >
+        <img
+          src="https://cdn.prod.website-files.com/65217fd9e31608b8b68141ba/653fe4735564be7de6ddfea8_6536e7fdf4c2c7c1471c11e5_64f8b11b97cdc0dfa55475eb_article%25252015-p-1080.png"
+          alt=""
+        />
+      </div>
+
+      <!-- writeup -->
+      <div
+        class="bg-gray-100 transit dark:bg-gray-600 rounded-3xl mt-8 md:mt-14 p-8 md:p-12 text-center md:text-left"
+      >
+        <p class="text-left">
+          <span
+            class="rounded-lg bg-blue-500 font-semibold text-sm text-slate-100 text-left p-2"
+          >
+            TDLR
+          </span>
+        </p>
+
+        <p class="mt-4 text-left">
+          An NFT (non-fungible token) is a unique digital item stored on a
+          blockchain. NFTs can represent almost anything, and serve as a digital
+          record of ownership.
+        </p>
+      </div>
+
+      <!-- plenty text -->
+      <div class="text-left mt-5">
+        <p class="font-bold text-3xl">Fungible vs. non-fungible</p>
+        <p class="mt-2">
+          Before we dive into NFTs, it’s important to understand the
+          “non-fungible” part of “non-fungible token.” When an item is fungible,
+          it means it’s interchangeable with another of the same item. A classic
+          example is a $1 dollar bill: you could swap dollars with someone and
+          you’d both still have $1.
+        </p>
+
+        <p class="mt-2">
+          Non-fungible, on the other hand, means the item is totally unique, and
+          therefore has its own unique value. For example, two cars of the same
+          make and model might have different values based on how many miles are
+          on the odometer, their accident records, or if it was previously owned
+          by a celebrity.
+        </p>
+
+        <p class="font-bold text-3xl mt-5">How do NFTs work?</p>
+
+        <p class="mt-2">
+          NFTs operate on blockchain technology. The blockchain is basically a
+          large, digital, public record. The most popular blockchains are
+          distributed across many nodes (read: people’s computers), which is why
+          you’ll hear them described as “decentralized.”
+        </p>
+        <p class="mt-2">
+          So instead of a central company-owned server, the blockchain is
+          distributed across a peer-to-peer network. Not only does this ensure
+          that the blockchain remains immutable, it also allows the node
+          operators to earn money, instead of a single company. Because the
+          blockchain records and preserves history, it is uniquely positioned to
+          transform provable authenticity and digital ownership.
+        </p>
+        <p class="mt-2">
+          When someone creates, transfers, buys, sells, or otherwise does
+          something with an NFT, that all gets recorded on the blockchain. This
+          is what enables authentication.
+        </p>
+        <p class="mt-2">
+          This record serves as a permanent statement of authenticity that can
+          be viewed or accessed by anyone. Today, when you buy a piece of art or
+          a collector's item, it typically comes with a paper certificate of
+          authenticity, which you must then keep track of forever. It is easily
+          forgotten, lost or destroyed, creating a very fragile system for
+          authenticity. Blockchain’s offer a simple and more secure solution to
+          this long standing issue of proving authenticity.
+        </p>
+
+        <p class="font-bold text-3xl mt-5">What are NFTs used for?</p>
+        <p class="mt-2">
+          An NFT can represent anything, but let’s explore some of the ways NFTs
+          are being used today, and potential implementations for the future.
+        </p>
+        <p class="font-bold text-2xl mt-5">Art NFTs</p>
+        <p class="mt-2">
+          An art NFT is a type of NFT that represents a piece of digital art,
+          such as a drawing, painting, or piece of digital artwork. Each art NFT
+          is unique and traceable to the original creator of the NFT, and that
+          connection to the creator may be valuable as well. Art NFTs are a new
+          form of digital art that can be collected and sold, similar to
+          physical artwork. Art NFTs can also have additional utility (for
+          example, owning the NFT may also give you commercial rights to use the
+          underlying artwork).
+        </p>
+        <p class="mt-2">
+          Artists are creating incredible and novel pieces with NFTs. Damien
+          Hirst used NFTs in his collection “The Currency”, in which he created
+          digital versions of 10,000 unique physical paintings. Collectors had
+          one year to decide if they wanted the digital or the physical version
+          of the painting— whichever version they did not choose would be
+          destroyed.
+        </p>
+        <p class="mt-2">
+          This record serves as a permanent statement of authenticity that can
+          be viewed or accessed by anyone. Today, when you buy a piece of art or
+          a collector's item, it typically comes with a paper certificate of
+          authenticity, which you must then keep track of forever. It is easily
+          forgotten, lost or destroyed, creating a very fragile system for
+          authenticity. Blockchain’s offer a simple and more secure solution to
+          this long standing issue of proving authenticity.
+        </p>
+
+        <p class="font-bold text-2xl mt-5">Profile picture NFTs (PFPs)</p>
+        <p class="mt-2">
+          A profile picture, or PFP, NFT is any NFT that's primary purpose is
+          use as a social media profile picture, or avatar. These NFTs are
+          digital items that are authenticated on a blockchain network, making
+          them verifiably unique. Some PFP NFTs also grant access to designated
+          online communities.
+        </p>
+        <p class="mt-2">
+          Profile picture NFTs come in various forms, ranging from static images
+          to animated or interactive designs. They may be based on characters,
+          take the form of animals, look like humans, or be abstract.
+        </p>
+        <p class="mt-2">
+          These are probably the projects you’ve heard the most about: Bored Ape
+          Yacht Club (BAYC), Doodles, World of Women (WoW), and more. For many
+          people on the internet, these PFPs actually become their online
+          identity. Not only do they identify with the group, they strongly
+          identify with their avatar. PFP projects are also centered around
+          holder benefits (like BAYC’s famous yacht party) and community (like
+          WoW, which donates a portion of their creator earnings to
+          women-centric charities).
+        </p>
+
+        <p class="font-bold text-2xl mt-5">Music NFTs</p>
+        <p class="mt-2">
+          A music NFT is any NFT that represents ownership of music or an
+          experience tied to music, like a live concert event or a virtual fan
+          meet-and-greet. Music NFTs are created by minting a unique token on a
+          blockchain, which can then be collected by music fans.
+        </p>
+        <p class="mt-2">
+          Music NFTs can be created by new artists and musicians with large
+          fanbases. These NFTs can include anything from a recording to a
+          composition to ticketing for a music experience. The content of each
+          music NFT varies based on the creator and the work they decide to
+          create.
+        </p>
+        <p class="mt-2">
+          Any artist can create a single song or audio file as an NFT. For
+          example, omgkirby partnered with Channel Tres to create a collection
+          of 5,550 unique songs, each with its own BPM and key. Each song comes
+          with vocals and production by Channel Tres.
+        </p>
+
+        <!--music image -->
+        <div
+          class="w-full h-full rounded-3xl mt-4 md:mt-7 overflow-hidden shadow-lg shadow-lime-200"
+        >
+          <img
+            src="https://cdn.prod.website-files.com/65217fd9e31608b8b68141ba/65217fd9e31608b8b68144a0_1d3f3fb08ab19fe6cac4ed9719c9bbdf.png"
+            alt=""
+          />
+        </div>
+
+        <section class="h-auto mt-5 p-7 md:p-12">
+          <main
+            class="grid md:grid-cols-2 items-center bg-slate-50 dark:bg-slate-800 transit rounded-2xl md:p-12 p-8"
+          >
+            <div class="w-full">
+              <p
+                class="shining md:uppercase text-3xl md:text-4xl text-left font-extrabold"
+              >
+                Create, Sell & Collect NFTs At Oceanic
+              </p>
+              <p class="text-gray-400 mt-5">
+                Created with the collaboration of over 60 of the world's best
+                Nuron Artists.
+              </p>
+
+              <div class="flex mt-8 gap-x-5 justify-center md:justify-start">
+                <d-button
+                  type="outlined"
+                  class="border-green-400 !text-green-400 active:!bg-green-300 dark:active:!bg-green-400"
+                  >Login</d-button
+                >
+                <d-button
+                  type="elevated"
+                  class="shadow-green-400 bg-green-400 dark:bg-green-500 text-white dark:!text-slate-900 active:!bg-green-300"
+                  to="/collection/red"
+                />
+              </div>
+            </div>
+            <div class="rounded-xl text-center mx-auto mt-8 md:mt-0">
+              <img
+                src="https://solanart.io/solanart-logo-black.svg"
+                alt=""
+                width="300"
+                class="animate__animated animate__tada animate__delay-5s animate__repeat-3 animate__slow"
+              />
+            </div>
+          </main>
+        </section>
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DButton from "@/components/utils/DButton.vue";
+</script>
 
-<style></style>
+<style>
+.shining {
+  text-align: center;
+  background: linear-gradient(
+    90deg,
+    #d71ef9 10%,
+    #1bee97 40%,
+    #0057ff 60%,
+    #d71ef9 90%
+  );
+  background-size: 200% auto;
+  color: #000;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-animation: shine 6s linear infinite;
+  animation-duration: 6s;
+  animation-timing-function: linear;
+  animation-delay: 0s;
+  animation-iteration-count: infinite;
+  animation-direction: normal;
+  animation-fill-mode: none;
+  animation-play-state: running;
+  animation-name: shine;
+}
+@keyframes shine {
+  100% {
+    background-position: 200%;
+    background-position-x: 200%;
+    background-position-y: center;
+  }
+}
+</style>
