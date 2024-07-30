a1a17a82416c0ee23f53f1277edebc5f0b10b437
swaedavid <swaedavid@gmail.com>
2024-07-30 03:05:47 +0100

    updated

--- src/views/dashboard/deposit/wallet.vue
+++ src/views/dashboard/deposit/wallet.vue
@@ -23,10 +23,20 @@
       </DDashbar>
 
       <section>
-        <div class="mt-3 text-slate-900 dark:text-slate-100">
-          <img src="@/assets/qrcode.png" alt="" class="mx-auto text-center" />
+        <div class="mt-5 text-slate-900 dark:text-slate-100">
+          <div class="mx-auto text-center">
+            <div
+              class="overflow-hidden rounded-lg border inline-block mx-auto text-center"
+            >
+              <img
+                src="@/assets/qrcode.png"
+                alt=""
+                class="mx-auto text-center"
+              />
+            </div>
+          </div>
 
-          <div class="border rounded-3xl dark:border-slate-500 p-4 mt-6">
+          <div class="rounded-3xl dark:border-slate-500 p-4 mt-6">
             <p class="opacity-55 text-sm">Network</p>
             <p class="opacity-85 font-semibold">Ethereum (ERC20)</p>
 
@@ -44,35 +54,40 @@
             <p class="opacity-85 font-semibold break-words">
               KJJjhJHlhLHbHJHhklJLBUIghjHy685tgiI76tgibkT6885rfuv
             </p>
-
-            <div
-              class="mt-2 p-3 rounded-xl bg-gray-200 dark:text-slate-100 text-slate-900 overflow-hidden dark:bg-slate-700 caret:slate-700 dark:caret-slate-100 h-[50px] has-[:focus]:ring-green-400 has-[:focus]:ring-1 outline-none transit relative z-0 flex items-center gap-x-3"
-            >
-              <div>
-                <SvgComp
-                  icon="M11.944 17.97L4.58 13.62 11.943 24l7.37-10.38-7.372 4.35h.003zM12.056 0L4.69 12.223l7.365 4.354 7.365-4.35L12.056 0z"
-                  Sclass="max-w-4"
-                  class="min-w-5"
-                />
-              </div>
-              <div class="relative w-full">
-                <input
-                  type="tel"
-                  name="amount"
-                  id="amount"
-                  class="block px-0 w-full mt-2 text-sm text-gray-900 bg-transparent border-0 appearance-none dark:text-white focus:outline-none focus:ring-0 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="amount"
-                  class="peer-focus:font-medium left-0 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-8 peer-focus:text-green-500 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:left-0"
-                  >Deposit Amount in (ETH)</label
-                >
-              </div>
-              <!-- class="absolute block top-2 text-gray-400 peer-focus:top-0 peer-focus:text-xs left-10 text-left peer-focus:left-8 transit" -->
+          </div>
+          <div
+            class="group mt-2 p-1 rounded-lg dark:border-gray-600 border bg-transparent dark:text-slate-100 text-slate-900 overflow-hidden caret:slate-700 dark:caret-slate-100 h-[50px] has-[:focus]:border-green-500 outline-none transit relative z-0 flex items-center gap-x-3"
+          >
+            <div>
+              <SvgComp
+                icon="M11.944 17.97L4.58 13.62 11.943 24l7.37-10.38-7.372 4.35h.003zM12.056 0L4.69 12.223l7.365 4.354 7.365-4.35L12.056 0z"
+                Sclass="group-has-[:focus]:stroke-green-500"
+                class=""
+              />
+            </div>
+            <div class="relative w-full">
+              <input
+                type="number"
+                name="amount"
+                v-model="amount"
+                id="amount"
+                class="block px-0 w-full mt-2 text-sm text-gray-900 bg-transparent border-0 appearance-none dark:text-white focus:outline-none focus:ring-0 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="amount"
+                class="w-96 peer-focus:w-96 peer-focus:font-medium left-0 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-8 peer-focus:text-green-500 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:left-0"
+                >Deposit Amount in (ETH)</label
+              >
             </div>
           </div>
+          <p
+            class="mt-1 text-xs font-light font-sans text-center"
+            :class="amount ? ' transit' : 'scale-y-0 transit '"
+          >
+            Dollar Conversion : {{ convertAmount }}
+          </p>
         </div>
       </section>
     </main>
@@ -82,11 +97,16 @@
 <script setup>
 import DDashbar from "@/components/utils/DDashbar.vue";
 import SvgComp from "@/components/svgComp.vue";
-import { inject, ref } from "vue";
+import { computed, inject, ref } from "vue";
 
 const theme = inject("theme");
 
 const amount = ref("");
+
+const convertAmount = computed(() => {
+  const dollars = Number(amount.value) * 3043;
+  return amount.value ? `$${dollars.toLocaleString()}` : "";
+});
 </script>
 
 <style></style>
