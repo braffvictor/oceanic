41172b9f4df51486e4c519e08cf836d803ad4821
swaedavid <swaedavid@gmail.com>
2024-09-07 04:16:27 +0100

    updated

--- src/views/admin/users/edituser.vue
+++ src/views/admin/users/edituser.vue
@@ -7,7 +7,7 @@
         <div class="flex justify-between items-center">
           <p class="text-base md:text-lg font-semibold">Bio Data</p>
 
-          <div class="text-right float-left border relative transit">
+          <div class="text-right float-left relative transit">
             <svg-comp
               icon="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM8 13C8.55228 13 9 12.5523 9 12C9 11.4477 8.55228 11 8 11C7.44772 11 7 11.4477 7 12C7 12.5523 7.44772 13 8 13ZM12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13ZM16 13C16.5523 13 17 12.5523 17 12C17 11.4477 16.5523 11 16 11C15.4477 11 15 11.4477 15 12C15 12.5523 15.4477 13 16 13Z"
               class="mx-auto text-center w-full peer"
@@ -15,10 +15,10 @@
             />
             <!-- options for nft menu -->
             <div
-              class="absolute shadow-md h-auto -left-44 md:left-4 transit top-10 md:text-base text-sm md:text- font-normal z-20 w-56 rounded-xl bg-slate-100 dark:bg-slate-800 flex flex-col divide-y p-1 scale-y-0 peer-hover:scale-y-100 -translate-y-full divide-slate-900 dark:divide-slate-100 divide-opacity-20 dark:divide-opacity-45 peer-hover:-translate-y-0 md:translate-y-0 opacity-0 peer-hover:opacity-100 gap-y-2 peer-hover:block py-2 hover:scale-100 hover:-translate-y-0 hover:block md:hover:opacity-100 text-start"
+              class="absolute shadow-md h-auto -left-44 md:left-5 transit top-5 md:top-3 md:text-base text-sm md:text- font-normal z-20 w-52 rounded-xl bg-slate-100 dark:bg-slate-800 flex flex-col divide-y p-1 scale-y-0 peer-hover:scale-y-100 -translate-y-full divide-slate-900 dark:divide-slate-100 divide-opacity-20 dark:divide-opacity-45 peer-hover:-translate-y-0 md:translate-y-0 opacity-0 peer-hover:opacity-100 gap-y-2 cursor-pointer peer-hover:block py-2 hover:scale-100 hover:-translate-y-0 hover:block md:hover:opacity-100 text-start"
             >
               <p
-                class="p-1 active:bg-slate-200 text-slate-900 dark:text-slate-100 dark:active:bg-slate-700"
+                class="p-1 py-3 px-2 active:bg-slate-200 hover:bg-slate-200 dark:text-slate-100 dark:hover:bg-slate-700 text-slate-900 dark:active:bg-slate-700"
                 @click="
                   $router.push(
                     `/admin/user/activities/${
@@ -30,7 +30,7 @@
                 {{ user && user.fullName }} Deposits
               </p>
               <p
-                class="p-1 active:bg-slate-200 text-slate-900 dark:text-slate-100 dark:active:bg-slate-700"
+                class="p-1 py-3 px-2 active:bg-slate-200 hover:bg-slate-200 dark:text-slate-100 dark:hover:bg-slate-700 text-slate-900 dark:active:bg-slate-700"
                 @click="
                   $router.push(
                     `/admin/user/activities/${
@@ -42,7 +42,7 @@
                 {{ user && user.fullName }} Withdrawals
               </p>
               <p
-                class="p-1 active:bg-slate-200 text-slate-900 dark:text-slate-100 dark:active:bg-slate-700"
+                class="p-1 py-3 px-2 active:bg-slate-200 hover:bg-slate-200 dark:text-slate-100 dark:hover:bg-slate-700 text-slate-900 dark:active:bg-slate-700"
                 @click="
                   $router.push(
                     `/admin/user/activities/${user && user.userID}?type=allnfts`
