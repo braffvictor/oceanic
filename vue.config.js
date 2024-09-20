4efb9faba44e3c428318918386e102688b737046
swaedavid <swaedavid@gmail.com>
2024-09-20 22:56:12 +0100

    updated

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,18 @@ module.exports = defineConfig({
     appleMobileWebAppCache: "yes",
     manifestOptions: {
       background_color: "#4ade80",
+      icons: [
+        {
+          src: "@/assets/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "@/assets/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
       name: "Oceanic",
       short_name: "Oceanic",
       theme_color: "#4ade80",
